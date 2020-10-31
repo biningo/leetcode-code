17f62c65a49807b4e12064c051958a8996dc5abb
biningo <biningo@yeah.net>
2020-10-31 08:55:24 +0800

    95|98|105|offer68

--- go/tree/Offer68-二叉树的最近公共祖先.go
+++ go/tree/Offer68-二叉树的最近公共祖先.go
@@ -7,23 +7,24 @@ package tree
 **/
 //https://leetcode-cn.com/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof/solution/mian-shi-ti-68-ii-er-cha-shu-de-zui-jin-gong-gon-7/
 
-func lowestCommonAncestor2(root, p, q *TreeNode) *TreeNode {
-	if root == nil || root == p || root == q {
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root==nil{
+		return nil
+	}
+	if root.Val==q.Val || root.Val==p.Val{
 		return root
 	}
 
-	left := lowestCommonAncestor2(root.Left, p, q)
-	right := lowestCommonAncestor2(root.Right, p, q)
-
-	//返回父亲节点
-	if left != nil && right != nil {
+	left := lowestCommonAncestor(root.Left,p,q)
+	right := lowestCommonAncestor(root.Right,p,q)
+	if left!=nil && right!=nil{
 		return root
 	}
-
-	if left != nil {
+	if left==nil && right==nil{
+		return nil
+	}else if left!=nil{
 		return left
-	}
-	if right != nil {
+	}else if right!=nil{
 		return right
 	}
 	return nil
